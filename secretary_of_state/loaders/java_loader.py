393bf188d9d13bfc9c8ed1a0aa52b753dc259f68
aidnem <99768676+aidnem@users.noreply.github.com>
2025-10-21 18:33:52 -0400

    (#5) Replace all usage of e with egex for consistency

--- secretary_of_state/loaders/java_loader.py
+++ secretary_of_state/loaders/java_loader.py
@@ -2,7 +2,6 @@ import sys
 import tomllib
 from attr import define
 
-import re
 import regex
 
 from secretary_of_state.loaders.base_loader import BaseLoader
@@ -16,7 +15,7 @@ class JavaLoader(BaseLoader):
         file_contents = self.load_if_present(lambda f: str(f.read(), encoding="utf-8"))
 
         # Find a state machine configuration
-        config_match = re.search(
+        config_match = regex.search(
             r"(\S+).*=.*new StateMachineConfiguration[\s\S]+new StateMachine",
             file_contents,
         )
@@ -31,19 +30,23 @@ class JavaLoader(BaseLoader):
         config_name = config_match.group(1)
 
         # Trim comments from the config
-        config_str = re.sub(r"\/\/.*$\n", "", config_str, flags=re.MULTILINE)
+        config_str = regex.sub(r"\/\/.*$\n", "", config_str, flags=regex.MULTILINE)
 
         # Trim the `new StateMachineConfiguration` line
-        config_str = re.sub(r"\S+.*=.*new StateMachineConfiguration.*;", "", config_str)
+        config_str = regex.sub(
+            r"\S+.*=.*new StateMachineConfiguration.*;", "", config_str
+        )
 
         # Trim the `new StateMachine` line
-        config_str = re.sub(r"^.*new StateMachine$", "", config_str, flags=re.MULTILINE)
+        config_str = regex.sub(
+            r"^.*new StateMachine$", "", config_str, flags=regex.MULTILINE
+        )
 
         # Remove all carriage returns
         # config_str = config_str.replace("\r", "")
 
         # Find the name of the state type and trigger type
-        state_type_match = re.search(r"\.configure\((.*)\..*\)", config_str)
+        state_type_match = regex.search(r"\.configure\((.*)\..*\)", config_str)
 
         if state_type_match is None:
             print(
@@ -55,7 +58,7 @@ class JavaLoader(BaseLoader):
         # Remove all occurrences of the state type
         config_str = config_str.replace(state_type_prefix, "")
 
-        trigger_type_match = re.search(r"\.permit(?:If)?\((\w+)", config_str)
+        trigger_type_match = regex.search(r"\.permit(?:If)?\((\w+)", config_str)
 
         if trigger_type_match is None:
             print(
@@ -71,10 +74,10 @@ class JavaLoader(BaseLoader):
 
         # Replace '[ConfigurationName].configure([State]) with [State] = \['
         config_str = config_str.replace(config_name, "")
-        config_str = re.sub(r"\.configure\((\S+)\)", r"\1 = [\n", config_str)
+        config_str = regex.sub(r"\.configure\((\S+)\)", r"\1 = [\n", config_str)
 
         # Convert all multi-line permit and permitIf statements to be single line
-        config_str = re.sub(
+        config_str = regex.sub(
             r"\.permit\((\w*?),\r?\n?\s*(\w*)\)", r".permit(\1, \2)", config_str
         )
 
@@ -82,44 +85,44 @@ class JavaLoader(BaseLoader):
             r"\.permitIf\(\r?\n?\s*(\S*),\r?\n?\s*(\S*)\,\r?\n?\s*((?:[^()]|(?:\((?3)\)))*)\r?\n?\)(;?)[\r\n]+",
             r".permitIf(\1, \2, <START_SECRETARY_CONDITION>\3<END_SECRETARY_CONDITION>)\4\n",
             config_str,
-            flags=re.MULTILINE,
+            flags=regex.MULTILINE,
         )
 
         # Remove newlines from between the SECRETARY_CONDITION tags
         while (
-            maybe_match := re.search(
+            maybe_match := regex.search(
                 "<START_SECRETARY_CONDITION>.*?<END_SECRETARY_CONDITION>",
                 config_str,
-                flags=re.DOTALL,
+                flags=regex.DOTALL,
             )
         ) is not None:
             new_text = maybe_match.group(0)
             new_text = new_text.replace("\r", "")
             new_text = new_text.replace("\n", "")
-            new_text = re.sub(r"\s+", " ", new_text)
+            new_text = regex.sub(r"\s+", " ", new_text)
             new_text = new_text.replace("<START_SECRETARY_CONDITION>", "")
             new_text = new_text.replace("<END_SECRETARY_CONDITION>", "")
             config_str = config_str.replace(maybe_match.group(0), new_text)
 
         # Flatten all indenting
-        config_str = re.sub(r"^\s*(\S)", r"\1", config_str, flags=re.MULTILINE)
+        config_str = regex.sub(r"^\s*(\S)", r"\1", config_str, flags=regex.MULTILINE)
 
         # Turn ; into a newline followed by the ending ] of the array
         config_str = config_str.replace(";", "\n]\n")
 
         # Turn permit statements into non-conditional transitions
-        config_str = re.sub(
+        config_str = regex.sub(
             r"\.permit\((\w*?),\s*(\w*)\)",
             r'    { trigger = "\1", destination = "\2" },',
             config_str,
         )
 
         # Turn permitIf statements into conditional transitions
-        config_str = re.sub(
+        config_str = regex.sub(
             r"\.permitIf\(\s*(\S*),\s*(\S*)\,\s*(.*)\r?\n?\)",
             r'    { trigger = "\1", condition = "\3", destination = "\2" },',
             config_str,
-            flags=re.MULTILINE,
+            flags=regex.MULTILINE,
         )
 
         # Remove the leading `() ->` from conditional transitions
